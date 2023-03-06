301f34b1f1c8079dcb436227aed68788ed037b70
mnahinkhan <mnk1@alumni.cmu.edu>
2023-03-06 15:01:38 +0300

    Try auto-installing autojump

--- install-tools.sh
+++ install-tools.sh
@@ -1,12 +1,18 @@
-# If OS is ubuntu install exa
+# If OS is ubuntu install exa and autojump
 
-# Source: https://lindevs.com/install-exa-on-ubuntu
 
 if [ -f /etc/os-release ] && [ "$(grep -c /etc/os-release "Ubuntu")" -ne 0 ]; then
+
+    # Source: https://lindevs.com/install-exa-on-ubuntu
     sudo apt update
     sudo apt install -y unzip
     EXA_VERSION=$(curl -s "https://api.github.com/repos/ogham/exa/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
     curl -Lo exa.zip "https://github.com/ogham/exa/releases/latest/download/exa-linux-x86_64-v${EXA_VERSION}.zip"
     sudo unzip -q exa.zip bin/exa -d /usr/local
     rm -rf exa.zip
+
+    # Source: https://github.com/wting/autojump
+    # OMZ plugin should help with making 'j' work
+    sudo apt-get install -y autojump
+
 fi
