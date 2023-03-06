468ece54becb710880d23f94e4053554183557a3
mnahinkhan <mnk1@alumni.cmu.edu>
2023-03-06 14:52:42 +0300

    Try auto-installing exa

---
+++ install-exa.sh
@@ -0,0 +1,12 @@
+# If OS is ubuntu install exa
+
+# Source: https://lindevs.com/install-exa-on-ubuntu
+
+if [ -f /etc/os-release ] && [ "$(grep -c /etc/os-release "Ubuntu")" -ne 0 ]; then
+    sudo apt update
+    sudo apt install -y unzip
+    EXA_VERSION=$(curl -s "https://api.github.com/repos/ogham/exa/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
+    curl -Lo exa.zip "https://github.com/ogham/exa/releases/latest/download/exa-linux-x86_64-v${EXA_VERSION}.zip"
+    sudo unzip -q exa.zip bin/exa -d /usr/local
+    rm -rf exa.zip
+fi
